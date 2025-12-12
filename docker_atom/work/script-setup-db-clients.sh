adf22ccdd07acce68b69ab4c49a18d4109ea1766
Bibo Hao <haobibo@users.noreply.github.com>
2025-12-12 07:36:08 +0000

    add oracle client install

--- docker_atom/work/script-setup-db-clients.sh
+++ docker_atom/work/script-setup-db-clients.sh
@@ -39,3 +39,30 @@ setup_redis_client() {
   sudo apt-get update && sudo apt-get -y install redis-tools ;
   type redis-cli && echo "@ Version of redis-cli: $(redis-cli --version)" || return -1 ;
 }
+
+
+setup_oracle_client() {
+  local URL="https://www.oracle.com/database/technologies/instant-client/linux-x86-64-downloads.html" ;
+
+  local BUILD=$(curl -s "$URL" | grep -oP 'otn_software/linux/instantclient/\K[0-9]+' | head -n1) ;
+  local VER=$(curl -s "$URL" | grep -oP 'instantclient-basic-linux\.x64-\K[0-9]+(?:\.[0-9]+)+' | head -n1) ;
+  [ -z "$BUILD" ] && echo "!! Cannot detect BUILD ID" && return 1 ;
+  [ -z "$VER" ] && echo "!! Cannot detect VERSION" && return 1 ;
+
+  URL="https://download.oracle.com/otn_software/linux/instantclient/${BUILD}/instantclient-basic-linux.x64-${VER}.zip" ;
+  echo "@ Downloading Oracle Client: ${VER} (BUILD ${BUILD}) from URL: ${URL}" ;
+  curl -L -o "/tmp/instantclient-basic-linux.zip" "$URL" ;
+
+  sudo mkdir -pv /opt/oracle ;
+  sudo unzip /tmp/instantclient-basic-linux*.zip "instantclient_*/*" -d /opt/oracle/ ;
+  sudo ln -sf /opt/oracle/instantclient_* /opt/oracle/instantclient ;
+
+  echo "/opt/oracle/instantclient" | sudo tee /etc/ld.so.conf.d/oracle-instantclient.conf >/dev/null ;
+  sudo apt-get install -y libaio1t64 ;
+  sudo ln -s /lib/x86_64-linux-gnu/libaio.so.1t64 /lib/x86_64-linux-gnu/libaio.so.1 ;
+  sudo ldconfig ;
+
+  (ls "/opt/oracle/instantclient/libclntsh.so"* "/opt/oracle/instantclient/libnnz"* "/opt/oracle/instantclient/libocci"* >/dev/null) \
+    && echo "@ Installed: /opt/oracle/instantclient" \
+    || echo "!! Missing client libraries" ;
+}
