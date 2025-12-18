fd85fb4cdd384901a720f55c332814385e509632
haobibo <haobibo@noreploy.github.com>
2025-12-19 03:16:43 +0800

    debug apt sources

--- docker_db_postgres/rootfs/opt/utils/script-setup-pg_ext_mirror.sh
+++ docker_db_postgres/rootfs/opt/utils/script-setup-pg_ext_mirror.sh
@@ -1,14 +1,38 @@
 export DISTRO_NAME=$(awk '{ print tolower($0) }' <<< $(lsb_release -is))
 export DISTRO_CODE_NAME=$(lsb_release -cs)
 
+APT_KEYRING_DIR="/etc/apt/keyrings"
+APT_SOURCE_DIR="/etc/apt/sources.list.d"
+
+add_apt_source() {
+  local key_url="$1"
+  local keyring_name="$2"
+  local list_url="$3"
+  local list_name="$4"
+  local keyring_path="${APT_KEYRING_DIR}/${keyring_name}"
+  local list_path="${APT_SOURCE_DIR}/${list_name}"
+
+  curl -fsSL "$key_url" | gpg --dearmor > "$keyring_path"
+  curl -fsSL "$list_url" > "$list_path"
+}
+
 # apt source for: https://packagecloud.io/citusdata/community
-curl -fsSL "https://packagecloud.io/citusdata/community/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/citusdata_community.gpg
-echo "deb https://packagecloud.io/citusdata/community/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/citusdata_community.list
+add_apt_source \
+  "https://packagecloud.io/citusdata/community/gpgkey" \
+  "citusdata_community-archive-keyring.gpg" \
+  "https://packagecloud.io/install/repositories/citusdata/community/config_file.list?os=${DISTRO_NAME}&dist=${DISTRO_CODE_NAME}&source=script" \
+  "citusdata_community.list"
 
 # apt source for: https://packagecloud.io/timescale/timescaledb
-curl -fsSL "https://packagecloud.io/timescale/timescaledb/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/timescale_timescaledb.gpg
-echo "deb https://packagecloud.io/timescale/timescaledb/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/timescale_timescaledb.list
+add_apt_source \
+  "https://packagecloud.io/timescale/timescaledb/gpgkey" \
+  "timescale_timescaledb.gpg" \
+  "https://packagecloud.io/install/repositories/timescale/timescaledb/config_file.list?os=${DISTRO_NAME}&dist=${DISTRO_CODE_NAME}&source=script" \
+  "timescale_timescaledb.list"
 
 # apt source for: https://packagecloud.io/pigsty/pgsql
-curl -fsSL "https://packagecloud.io/pigsty/pgsql/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/pigsty_pgsql.gpg
-echo "deb https://packagecloud.io/pigsty/pgsql/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/pigsty_pgsql.list
+add_apt_source \
+  "https://packagecloud.io/pigsty/pgsql/gpgkey" \
+  "pigsty_pgsql.gpg" \
+  "https://packagecloud.io/install/repositories/pigsty/pgsql/config_file.list?os=${DISTRO_NAME}&dist=${DISTRO_CODE_NAME}&source=script" \
+  "pigsty_pgsql.list"
