d8fb8db4b99bf2029b27e866e6e4eee2cabf0a77
Peter Howe <haobibo@users.noreply.github.com>
2025-12-19 17:41:00 +0800

    debug pg ext install

--- docker_db_postgres/rootfs/docker-entrypoint-initdb.d/01-init-db.sh
+++ docker_db_postgres/rootfs/docker-entrypoint-initdb.d/01-init-db.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-set -Eeo pipefail
+set -euo pipefail
 
+# 1. dynamically update preload-extensions
+PRELOAD_LIBS="${PG_PRELOAD_LIBS:-pg_cron}"
+CRON_DB="${POSTGRES_DB:-postgres}"
+CONF_FILE="${PGDATA}/conf.d/20-preload-extensions.conf"
+
+echo "Configuring shared_preload_libraries in ${CONF_FILE} to: ${PRELOAD_LIBS}"
+
+cat > "${CONF_FILE}" <<EOF
+# This file is generated at initdb time. Do NOT edit manually!
+#
+# Available preload extensions:
+# citus,timescaledb,pg_stat_statements,auto_explain,pg_cron,
+# pg_partman_bgw,pgaudit,pgautofailover,pg_qualstats,pg_squeeze
+
+shared_preload_libraries = '${PRELOAD_LIBS}'
+
+# pg_cron
+cron.database_name = '${CRON_DB}'
+EOF
+
+# 2. print system info for log and debug
 printenv | sort
-
 ls -alh /usr/share/postgresql/${PG_MAJOR}/extension/*.control
-
-mkdir -pv ${PGDATA}/conf.d
-echo "include_dir='./conf.d'" >> ${PGDATA}/postgresql.conf
 tail ${PGDATA}/postgresql.conf
-
-cat <<EOT >> ${PGDATA}/conf.d/20-preload-extensions.conf
-# citus,timescaledb,pg_stat_statements,auto_explain,pg_cron,pg_partman_bgw
-shared_preload_libraries = 'pgaudit,pgautofailover,pg_qualstats,pg_squeeze'
-cron.database_name='${POSTGRES_DB:-postgres}'
-EOT
 cat ${PGDATA}/conf.d/*
 
-# pg_analytics
+# pg_analytics and some ext may requires system restart
 
-# https://github.com/docker-library/postgres/blob/master/docker-entrypoint.sh
-# form: docker-entrypoint.sh
+# form `docker-entrypoint.sh`: https://github.com/docker-library/postgres/blob/master/docker-entrypoint.sh
 docker_temp_server_stop
 sleep 2s
 docker_temp_server_start
 
 
-psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CALL enable_all_extensions();
-    
-    SELECT a.name, e.extversion AS installed, a.default_version AS avaliable, a.comment --e.extowner, e.extnamespace, e.extrelocatable
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL    
+    SELECT a.name, e.extversion AS installed, a.default_version AS avaliable, a.comment -- e.extowner, e.extnamespace, e.extrelocatable
     FROM pg_available_extensions AS a LEFT JOIN pg_extension AS e ON a.name = e.extname
     ORDER BY name;
 EOSQL
