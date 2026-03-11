86976e655f45a9b365f90c3e459728cae8ed45aa
haobibo <haobibo@noreploy.github.com>
2026-03-11 18:11:39 +0800

    debug clean script

--- docker_atom/work/script-utils.sh
+++ docker_atom/work/script-utils.sh
@@ -46,7 +46,9 @@ install__clean(){
   type npm     && npm cache clean --force ;
   type mamba   && mamba clean -ya && rm -rf ~/micromamba ;
   type conda   && conda clean -ya && ( rm -rf "${CONDA_PREFIX:-/opt/conda}"/pkgs/* || true ) ;
-  find "${CONDA_PREFIX:-/opt/conda}"/lib \( -name "__pycache__" -o -name "*.pyc" -o -name "*.pyo" \) -delete 2>/dev/null ;
+  find "${CONDA_PREFIX:-/opt/conda}"/lib -type f -name "*.py[co]" -delete 2>/dev/null ;
+  find "${CONDA_PREFIX:-/opt/conda}"/lib -type d -name "__pycache__" -prune -exec rm -rf {} + 2>/dev/null ;
+
   ( rm -rf /usr/share/doc /usr/share/man || true ) ;
   ( rm -rf /tmp/.* /tmp/* /var/log/* /var/cache/* /root/.cache /root/.* || true ) && chmod ugo+rwXt /tmp ;
   ls -alh /root /tmp ;
